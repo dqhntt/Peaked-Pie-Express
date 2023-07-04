3f6bff07c5e0c30ccaeeaa058bdeaa8d85ecdc6a
dqhntt <48117687+dqhntt@users.noreply.github.com>
2023-07-04 01:21:36 -0700

    Reorganize files into folders
    Prefabs and scripts to their new folders.
    Remove redundant music folder.
    Move animations folder up by one.

---
+++ Assets/Scripts/SnowFlakeMover.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowFlakeMover : MonoBehaviour
+{
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position = new Vector3(0, (-6 + player.transform.position.y), 0);
+        Debug.Log("snow pos " + transform.position);
+        Debug.Log("player pos " + player.transform.position);
+    }
+}
